a0ee3fac1209cb62421e73e4aff2cb84e61d2a51
Pedro Henrique Cunha Moda <phcunha47@gmail.com>
2023-09-06 01:06:44 -0300

    quarta_feira

--- ep/a05/a05-04.c
+++ ep/a05/a05-04.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-
-int find_number(int *vet, int i, int number){
-    if(vet[0]=number) return 1;
-    for(int i = 0; i < number; i++){
-        if(vet[i]==number){
-            return i;
+int find_number(int *vet, int i, int number) {
+    if (vet[0] == number) return 1;
+    for (int j = 0; j < i; j++) {
+        if (vet[j] == number) {
+            return j;
         }
     }
-    printf("O numero nao foi encontrado");
+    printf("O numero nao foi encontrado\n");
     return 0;
 }
 
-
-int main(){
+int main() {
     int *vet, i;
     i = 8;
     vet = malloc(i * sizeof(int));
+    vet[0] = 0;
+    vet[1] = 3;
+    vet[2] = 6;
+    vet[3] = 2;
+    vet[4] = 9;
+    vet[5] = 7;
+    vet[6] = 0;
+    vet[7] = 6;
     printf("Numero de comparações no melhor caso: %d\n", find_number(vet, i, 0));
-    printf("Numero de comparações no pior caso: %d\n", find_number(vet, i, vet[i-1]));
-    
+    printf("Numero de comparações no pior caso: %d\n", find_number(vet, i, 7));
+    free(vet);
+    return 0;
 }

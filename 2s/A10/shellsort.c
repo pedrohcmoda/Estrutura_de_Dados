894b880ff603648d6eea68d385c63c8a4414185c
pedrohcmoda <phcunha47@gmail.com>
2023-10-23 22:47:31 -0300

    shellsort

---
+++ 2s/A10/shellsort.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void shellsort(int array[], int valor) {
+    for (int gap = valor / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < valor; i++) {
+            int temp = array[i];
+            int j;
+            for (j = i; j >= gap && array[j - gap] > temp; j -= gap) {
+                array[j] = array[j - gap];
+            }
+            array[j] = temp;
+        }
+    }
+}
+
+int main() {
+    int array[] = {3, 4, 9, 2, 5, 1, 8};
+    printf("Array antes da implementação: \n");
+    for (int i = 0; i < 7; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+    shellsort(array, 7);
+    printf("Array depois da implementação: \n");
+    for (int i = 0; i < 7; i++) {
+        printf("%d ", array[i]);
+    }
+    return 0;
+}
